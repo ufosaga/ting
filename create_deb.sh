585026dac313d689e2190f94f537ad77ee6d0f6f
igagis <igagis@1aea46d3-b44b-0410-97f0-8bd0331b130a>
2008-11-22 09:58:44 +0000

    fix to create_deb.sh

--- create_deb.sh
+++ create_deb.sh
@@ -4,13 +4,7 @@ dst="debian_package/usr/include/ting"
 
 mkdir -p $dst
 
-cp src/debug.hpp $dst
-cp src/Exc.hpp $dst
-cp src/gendef.hpp $dst
-cp src/Ptr.hpp $dst
-cp src/Thread.hpp $dst
-cp src/ting.hpp $dst
-cp src/types.hpp $dst
+cp src/*.hpp $dst
 
 cp -r DEBIAN debian_package
 
