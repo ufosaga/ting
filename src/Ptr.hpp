626cb0aed4bca033ef448b1aa8648a9e94e58eac
igagis <igagis@1aea46d3-b44b-0410-97f0-8bd0331b130a>
2008-11-23 11:45:13 +0000

    comment with homepage address added to each file

--- src/Ptr.hpp
+++ src/Ptr.hpp
@@ -20,7 +20,8 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE. */
 
-// ting 0.2
+// ting 0.3
+// Homepage: http://code.google.com/p/ting
 // Author: Ivan Gagis <igagis@gmail.com>
 
 // File description:
