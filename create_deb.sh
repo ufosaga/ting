dad6e558168610646fbfcbbf07c9298c4cc55fe7
igagis <igagis@1aea46d3-b44b-0410-97f0-8bd0331b130a>
2009-11-06 14:53:06 +0000

    updateing debian-related files for packaging into deb packet

--- create_deb.sh
+++ create_deb.sh
@@ -4,8 +4,8 @@ dst="debian_package/usr/include/ting"
 
 mkdir -p $dst
 
-cp src/*.hpp $dst
+cp src/ting/*.hpp $dst
 
 cp -r DEBIAN debian_package
 
-dpkg -b debian_package libting-dev_0.2_i386.deb
+dpkg -b debian_package libting-dev_0.3.0-1_i386.deb
