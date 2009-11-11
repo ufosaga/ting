6e38dfb30bc63423bbb99055b407e02b2d341f9b
igagis <igagis@1aea46d3-b44b-0410-97f0-8bd0331b130a>
2009-11-11 10:25:09 +0000

    Thread_test added

---
+++ test/src/Thread_test.cpp
@@ -0,0 +1,52 @@
+#include <ting/debug.hpp>
+#include <ting/Thread.hpp>
+
+
+class TestThread : public ting::Thread{
+public:
+	int a, b;
+
+	//override
+	void Run(){
+		this->a = 10;
+		this->b = 20;
+		ting::Thread::Sleep(1000);
+		this->a = this->b;
+	}
+};
+
+
+
+static void TestJoinAfterThreadHasFinished(){
+	TestThread t;
+
+	t.Start();
+
+	ting::Thread::Sleep(2000);
+
+	t.Join();
+}
+
+
+
+static void TestJoinBeforeThreadHasFinished(){
+	TestThread t;
+
+	t.Start();
+
+	t.Join();
+}
+
+
+
+int main(int argc, char *argv[]){
+	TRACE(<< "Thread test" << std::endl)
+
+	TestJoinAfterThreadHasFinished();
+	
+	TestJoinBeforeThreadHasFinished();
+
+	TRACE(<< "[PASSED]: Thread test" << std::endl)
+
+	return 0;
+}
