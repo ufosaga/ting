ee0d664c5c3efa8c7b6d0edc53772117b6171012
igagis <igagis@1aea46d3-b44b-0410-97f0-8bd0331b130a>
2011-09-19 14:31:50 +0000

    debian scripts for binary file generation added

---
+++ debian/libting.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+soName=0
+
+packageName=libting$soName
+
+baseDir=debian/out/$packageName
+mkdir -p $baseDir
+
+#copy files
+libDir=$baseDir/usr/lib
+mkdir -p $libDir
+
+libFileName=libting.so
+
+cp src/$libFileName.$soName $libDir
+strip -g $libDir/$libFileName.$soName
+
+
+
+#create dir where the output 'control' will be placed
+mkdir -p $baseDir/DEBIAN
+
+#remove substvars
+rm -f debian/substvars
+
+#calculate dependancies
+dpkg-shlibdeps $libDir/$libFileName.$soName
+
+#generate final control file
+dpkg-gencontrol -p$packageName -P$baseDir
+
+dpkg -b $baseDir tmp-package.deb
+dpkg-name -o -s .. tmp-package.deb #rename file to proper debian format (package_version_arch.deb)
