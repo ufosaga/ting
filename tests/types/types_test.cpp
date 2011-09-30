3932858e62064058bb69719df41a8d2158e171ed
igagis <igagis@1aea46d3-b44b-0410-97f0-8bd0331b130a>
2011-09-30 14:12:46 +0000

    ting::Inited improvement for valatile types

--- tests/types/types_test.cpp
+++ tests/types/types_test.cpp
@@ -56,6 +56,50 @@ int main(int argc, char *argv[]){
 		ASSERT_ALWAYS(a == b)
 	}
 
+	//test operator=(const Inited&)
+	{
+		ting::Inited<int, 10> a;
+		ASSERT_ALWAYS(a == 10)
+		
+		ting::Inited<int, 20> b;
+		ASSERT_ALWAYS(b == 20)
+		
+		a = b;
+		ASSERT_ALWAYS(a == 20)
+		ASSERT_ALWAYS(b == a)
+	}
+	
+	//test Inited<volatile bool, false> assignment.
+	//GCC throws a warning when a function returning a reference to volatile is called in a void context
+	//If Inited::operator= returns just T& then we get that warning. Thus, made it to return Inited&
+	{
+		ting::Inited<volatile bool, false> v;
+		ASSERT_ALWAYS(v == false)
+		
+		bool value = true;
+		
+		v = value;
+		ASSERT_ALWAYS(v == true)
+		
+		value = false;
+		
+		bool anotherValue = true;
+		
+		(v = value) = anotherValue;
+		ASSERT_ALWAYS(v == true)
+		
+		v = false;
+		ASSERT_ALWAYS(v == false)
+		
+		ting::Inited<volatile bool, true> v2;
+		
+		v = v2;
+		ASSERT_ALWAYS(v == true)
+		
+		(v = value) = v2;
+		ASSERT_ALWAYS(v == true)
+	}
+	
 	TRACE_ALWAYS(<< "[PASSED]: types test" << std::endl)
 
 	return 0;
