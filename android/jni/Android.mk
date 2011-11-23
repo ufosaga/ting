39693f33dcbf68920ced160d3e83548f2cb580b9
igagis <igagis@1aea46d3-b44b-0410-97f0-8bd0331b130a>
2011-11-23 00:23:16 +0000

    adding Thread.cpp to compilation for android

--- android/jni/Android.mk
+++ android/jni/Android.mk
@@ -3,7 +3,10 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE    := native-activity
+
 LOCAL_SRC_FILES := main.cpp
+LOCAL_SRC_FILES += ting/Thread.cpp
+
 LOCAL_LDLIBS    := -llog -landroid -lEGL -lGLESv1_CM
 LOCAL_STATIC_LIBRARIES := android_native_app_glue
 
