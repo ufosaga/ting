60c2b7c47e7f87031839df119b9ed4b81d1a3012
igagis <igagis@1aea46d3-b44b-0410-97f0-8bd0331b130a>
2011-12-19 15:59:11 +0000

    some changes

--- android/jni/main.cpp
+++ android/jni/main.cpp
@@ -33,6 +33,136 @@
 #define LOGI(...) ((void)__android_log_print(ANDROID_LOG_INFO, "native-activity", __VA_ARGS__))
 #define LOGW(...) ((void)__android_log_print(ANDROID_LOG_WARN, "native-activity", __VA_ARGS__))
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+namespace TestFetchAndAdd{
+
+const unsigned DNumOps = 0xffff;
+
+class Thread : public ting::Thread{
+	ting::atomic::S32 &a;
+public:
+	Thread(ting::atomic::S32 &a) :
+			a(a)
+	{}
+	
+	ting::Semaphore sema;
+	
+	//override
+	void Run(){
+//		TRACE(<< "Thread::Run(): enter" << std::endl)
+		
+		//wait for start signal
+		this->sema.Wait();
+		
+		for(unsigned i = 0; i < DNumOps; ++i){
+			this->a.FetchAndAdd(1);
+		}
+	}
+};
+
+
+void Run(){
+	ting::atomic::S32 a;
+	
+	ting::StaticBuffer<ting::Ptr<Thread>, 100> threads;
+	
+	//Create and start all the threads
+	for(ting::Ptr<Thread>* i = threads.Begin(); i != threads.End(); ++i){
+		(*i) = ting::Ptr<Thread>(new Thread(a));
+		(*i)->Start();
+	}
+	
+	//wait till all the threads enter their Run() methods and start waiting on the semaphores
+	ting::Thread::Sleep(500);
+	
+	//signal all threads semaphores
+//	TRACE(<< "Signalling..." << std::endl)
+	for(ting::Ptr<Thread>* i = threads.Begin(); i != threads.End(); ++i){
+		(*i)->sema.Signal();
+	}
+	
+	//wait for all threads to finish
+	for(ting::Ptr<Thread>* i = threads.Begin(); i != threads.End(); ++i){
+		(*i)->Join();
+	}
+//	TRACE(<< "All threads finished" << std::endl)
+	
+	//Check atomic value
+	ASSERT_ALWAYS(a.FetchAndAdd(0) == ting::s32(DNumOps * threads.Size()))
+}
+}//~namespace
+
+
+
+namespace TestCompareAndExchange{
+void Run(){
+	ting::atomic::S32 a(10);
+	
+	ASSERT_ALWAYS(a.CompareAndExchange(10, 9) == 10)
+	ASSERT_ALWAYS(a.CompareAndExchange(9, 10) == 9)
+	
+	
+}
+}//~namespace
+
+
+
+namespace TestFlag{
+void Run(){
+	ting::atomic::Flag f;
+	
+	ASSERT_ALWAYS(f.Get() == false)
+	
+	ASSERT_ALWAYS(f.Set(false) == false)
+	ASSERT_ALWAYS(f.Set(true) == false)
+	ASSERT_ALWAYS(f.Set(true) == true)
+	ASSERT_ALWAYS(f.Set(false) == true)
+}
+}//~namespace
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /**
  * Our saved state data.
  */
@@ -260,6 +390,12 @@ void android_main(struct android_app* state) {
 	}catch(std::exception& e){
 		TRACE_ALWAYS(<< "exception caught: " << e.what() << std::endl)
 	}
+	
+	
+	
+	TestFlag::Run();
+	TestCompareAndExchange::Run();
+	TestFetchAndAdd::Run();
 	
 	
 	
