9c50b21bdad0919b2de52acf822633f50a9fc276
igagis <igagis@1aea46d3-b44b-0410-97f0-8bd0331b130a>
2011-12-26 16:48:20 +0000

    Inited testing

--- tests/types/main.cpp
+++ tests/types/main.cpp
@@ -105,9 +105,185 @@ void Run(){
 }//~namespace
 
 
-int main(int argc, char *argv[]){
 
+namespace TestOperators{
+struct TestClass{
+	int g;
+
+	TestClass() :
+			g(14)
+	{}
+};
+
+void Run(){
+	//operator=(Inited)
+	{
+		ting::Inited<int, 10> a;
+		ASSERT_ALWAYS(a == 10)
+		
+		ting::Inited<int, 20> b;
+		ASSERT_ALWAYS(b == 20)
+		
+		a = b;
+		ASSERT_ALWAYS(a == 20)
+		ASSERT_ALWAYS(b == a)
+	}
+	
+	//opertor=() with volatile value.
+	//test Inited<volatile bool, false> assignment.
+	//GCC throws a warning when a function returning a reference to volatile is called in a void context
+	//If Inited::operator= returns just T& then we get that warning. Thus, made it to return Inited&
+	{
+		ting::Inited<volatile bool, false> v;
+		ASSERT_ALWAYS(v == false)
+		
+		bool value = true;
+		
+		v = value;
+		ASSERT_ALWAYS(v == true)
+		
+		value = false;
+		
+		bool anotherValue = true;
+		
+		(v = value) = anotherValue;
+		ASSERT_ALWAYS(v == true)
+		
+		v = false;
+		ASSERT_ALWAYS(v == false)
+		
+		ting::Inited<volatile bool, true> v2;
+		
+		v = v2;
+		ASSERT_ALWAYS(v == true)
+		
+		(v = value) = v2;
+		ASSERT_ALWAYS(v == true)
+		
+		ting::Inited<bool, false> v3;
+		
+		
+		v = v3;
+		ASSERT_ALWAYS(v == false)
+		
+		(v = value) = v3;
+		ASSERT_ALWAYS(v == false)
+	}
+	
+	//operator->()
+	{
+		const ting::Inited<TestClass*, 0> p;
+		
+		p = new TestClass();
+		
+		ASSERT_ALWAYS(p->g == 14)
+		p->g = 15;
+		ASSERT_ALWAYS(p->g == 15)
+		
+		delete p;
+	}
+	
+	//operator->()const
+	{
+		ting::Inited<const TestClass*, 0> p;
+		
+		p = new TestClass();
+		
+		ASSERT_ALWAYS(p->g == 14)
+		
+		delete p;
+	}
+	
+	//operator+=
+	{
+		ting::Inited<int, 10> a;
+		
+		a += 13;
+		ASSERT_ALWAYS(a == 23)
+		
+		const ting::Inited<int, 20> a1;
+		
+		a1 += 13;
+		ASSERT_ALWAYS(a1 == 33)
+		
+		const ting::Inited<const int, 30> a2;
+		
+		ASSERT_ALWAYS(a2 == 30)
+	}
+	
+	//operator-=
+	{
+		ting::Inited<int, 10> a;
+		
+		a -= 13;
+		ASSERT_ALWAYS(a == -3)
+		
+		const ting::Inited<int, 20> a1;
+		
+		a1 -= 13;
+		ASSERT_ALWAYS(a1 == 7)
+	}
+	
+	//operator*=
+	{
+		ting::Inited<int, 10> a;
+		
+		a *= 3;
+		ASSERT_ALWAYS(a == 30)
+		
+		const ting::Inited<int, 20> a1;
+		
+		a1 *= 4;
+		ASSERT_ALWAYS(a1 == 80)
+	}
+	
+	//operator++()
+	{
+		ting::Inited<int, 10> a;
+		
+		++a;
+		ASSERT_ALWAYS(a == 11)
+		int ares = ++a;
+		ASSERT_ALWAYS(ares == 12)
+		++a = 35;
+		ASSERT_ALWAYS(a == 35)
+				
+		const ting::Inited<int, 20> a1;
+		
+		++a1;
+		ASSERT_ALWAYS(a1 == 21)
+		int a1res = ++a1;
+		ASSERT_ALWAYS(a1res == 22)
+	}
+	
+	//operator++(int)
+	{
+		ting::Inited<int, 10> a;
+		
+		a++;
+		ASSERT_ALWAYS(a == 11)
+		int ares = a++;
+		ASSERT_ALWAYS(ares == 11)
+		ASSERT_ALWAYS(a == 12)
+				
+		const ting::Inited<int, 20> a1;
+		
+		a1++;
+		ASSERT_ALWAYS(a1 == 21)
+		int a1res = a1++;
+		ASSERT_ALWAYS(a1res == 21)
+		ASSERT_ALWAYS(a1 == 22)
+	}
+	
+}	
+}//~namespace
+
+
+
+int main(int argc, char *argv[]){
+	
 	TestReferenceToInited::Run();
+	TestOperators::Run();
 	
 	{
 		ting::Inited<int, -10> a;
@@ -159,50 +335,6 @@ int main(int argc, char *argv[]){
 		ASSERT_ALWAYS(a == -10)
 		ASSERT_ALWAYS(b == -10)
 		ASSERT_ALWAYS(a == b)
-	}
-
-	//test operator=
-	{
-		ting::Inited<int, 10> a;
-		ASSERT_ALWAYS(a == 10)
-		
-		ting::Inited<int, 20> b;
-		ASSERT_ALWAYS(b == 20)
-		
-		a = b;
-		ASSERT_ALWAYS(a == 20)
-		ASSERT_ALWAYS(b == a)
-	}
-	
-	//test Inited<volatile bool, false> assignment.
-	//GCC throws a warning when a function returning a reference to volatile is called in a void context
-	//If Inited::operator= returns just T& then we get that warning. Thus, made it to return Inited&
-	{
-		ting::Inited<volatile bool, false> v;
-		ASSERT_ALWAYS(v == false)
-		
-		bool value = true;
-		
-		v = value;
-		ASSERT_ALWAYS(v == true)
-		
-		value = false;
-		
-		bool anotherValue = true;
-		
-		(v = value) = anotherValue;
-		ASSERT_ALWAYS(v == true)
-		
-		v = false;
-		ASSERT_ALWAYS(v == false)
-		
-		ting::Inited<volatile bool, true> v2;
-		
-		v = v2;
-		ASSERT_ALWAYS(v == true)
-		
-		(v = value) = v2;
-		ASSERT_ALWAYS(v == true)
 	}
 	
 	TRACE_ALWAYS(<< "[PASSED]: types test" << std::endl)
