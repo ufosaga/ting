0a463a736435af42806c69eb1d1c65854c4c69d8
igagis <igagis@1aea46d3-b44b-0410-97f0-8bd0331b130a>
2011-12-28 11:12:08 +0000

    static lib

--- android/jni/test/Android.mk
+++ android/jni/test/Android.mk
@@ -16,9 +16,7 @@ LOCAL_C_INCLUDES := $(LOCAL_PATH)/../ting
 
 LOCAL_LDLIBS    := -llog -landroid -lEGL -lGLESv1_CM
 
-LOCAL_SHARED_LIBRARIES := ting
-
-LOCAL_STATIC_LIBRARIES := android_native_app_glue
+LOCAL_STATIC_LIBRARIES := android_native_app_glue ting
 
 include $(BUILD_SHARED_LIBRARY)
 
