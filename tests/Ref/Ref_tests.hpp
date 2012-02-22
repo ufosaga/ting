a2e8c5938be56619f63c581ff5b89012f7fb8af3
igagis <igagis@1aea46d3-b44b-0410-97f0-8bd0331b130a>
2012-02-22 22:35:50 +0000

    tests refactoring

---
+++ tests/Ref/Ref_tests.hpp
@@ -0,0 +1,44 @@
+#pragma once
+
+namespace TestBoolRelatedStuff{
+void TestConversionToBool();
+void TestOperatorLogicalNot();
+}//~namespace
+
+namespace TestOperatorArrowAndOperatorStar{
+void Run1();
+void Run2();
+}//~namespace
+
+namespace TestBasicWeakRefUseCase{
+void Run1();
+void Run2();
+}//~namespace
+
+namespace TestExceptionThrowingFromRefCountedDerivedClassConstructor{
+void Run();
+}//~namespace
+
+namespace TestCreatingWeakRefFromRefCounted{
+void Run1();
+void Run2();
+}//~namespace
+
+namespace TestAutomaticDowncasting{
+void Run1();
+}//~namespace
+
+
+namespace TestVirtualInheritedRefCounted{
+void Run1();
+void Run2();
+void Run3();
+}//~namespace
+
+namespace TestConstantReferences{
+void Run1();
+}//~namespace
+
+namespace TestOverloadedOperatorDelete{
+void Run();
+}//~namespace
---
+++ tests/Ref/main.hpp
@@ -0,0 +1,32 @@
+#include "../../src/ting/debug.hpp"
+
+#include "Ref_tests.hpp"
+
+
+inline void TestTingRef(){
+	TestOperatorArrowAndOperatorStar::Run1();
+	TestOperatorArrowAndOperatorStar::Run2();
+
+	TestAutomaticDowncasting::Run1();
+
+	TestBoolRelatedStuff::TestConversionToBool();
+	TestBoolRelatedStuff::TestOperatorLogicalNot();
+	
+	TestBasicWeakRefUseCase::Run1();
+	TestBasicWeakRefUseCase::Run2();
+
+	TestExceptionThrowingFromRefCountedDerivedClassConstructor::Run();
+	
+	TestCreatingWeakRefFromRefCounted::Run1();
+	TestCreatingWeakRefFromRefCounted::Run2();
+	
+	TestVirtualInheritedRefCounted::Run1();
+	TestVirtualInheritedRefCounted::Run2();
+	TestVirtualInheritedRefCounted::Run3();
+
+	TestConstantReferences::Run1();
+	
+	TestOverloadedOperatorDelete::Run();
+
+	TRACE_ALWAYS(<< "[PASSED]: Ref test" << std::endl)
+}
