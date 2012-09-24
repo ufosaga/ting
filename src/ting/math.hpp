77b0dfe03206c2cdc2ca798184156a4d1944b9b4
igagis <igagis@1aea46d3-b44b-0410-97f0-8bd0331b130a>
2012-09-24 18:02:07 +0000

    ting::math::Round() function

--- src/ting/math.hpp
+++ src/ting/math.hpp
@@ -119,6 +119,27 @@ template <> inline double Floor<double>(double n)throw(){
 
 
 /**
+ * @brief Round number to closest integer.
+ * @param n - value to round.
+ * @return Rounded value.
+ */
+template <typename T> inline T Round(T n)throw();
+
+
+
+template <> inline float Round<float>(float n)throw(){
+	return ::round(n);
+}
+
+
+
+template <> inline double Round<double>(double n)throw(){
+	return ::round(n);
+}
+
+
+
+/**
  * @brief Get number Pi.
  * @return number Pi.
  */
