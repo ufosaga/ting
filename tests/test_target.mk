2e3ff799f5d795dd7c7e28c7fa8413447527a198
igagis <igagis@1aea46d3-b44b-0410-97f0-8bd0331b130a>
2013-06-12 09:40:55 +0000

    gdb target

--- tests/test_target.mk
+++ tests/test_target.mk
@@ -9,3 +9,17 @@ else
 	@LD_LIBRARY_PATH=../../src ./$^
     endif
 endif
+
+
+
+gdb: $(binary_name)
+	@echo gdb debugging $^...
+ifeq ($(platform),windows)
+	@gdb ./$^
+else
+    ifeq ($(platform),macosx)
+	@DYLD_LIBRARY_PATH=../../src gdb ./$^
+    else
+	@LD_LIBRARY_PATH=../../src gdb ./$^
+    endif
+endif
