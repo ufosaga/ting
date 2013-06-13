04df3964709236680e0cdfc0c7ae38416314468f
igagis <igagis@1aea46d3-b44b-0410-97f0-8bd0331b130a>
2013-06-13 12:05:54 +0000

    fix

--- src/ting/WaitSet.cpp
+++ src/ting/WaitSet.cpp
@@ -77,7 +77,7 @@ void WaitSet::Add(Waitable* w, Waitable::EReadinessFlags flagsToWaitFor){
 	ASSERT(revents.Size() < this->NumWaitables())
 	
 	int16_t filter = (u32(flagsToWaitFor) & Waitable::READ != 0 ? EVFILT_READ : 0)
-			| (u32(flagsToWaitFor) & Waitable::READ != 0 ? EVFILT_WRITE : 0);
+			| (u32(flagsToWaitFor) & Waitable::WRITE != 0 ? EVFILT_WRITE : 0);
 	
 	struct kevent e;
 
@@ -150,7 +150,7 @@ void WaitSet::Change(Waitable* w, Waitable::EReadinessFlags flagsToWaitFor){
 	}
 #elif M_OS == M_OS_MACOSX
 	int16_t filter = (u32(flagsToWaitFor) & Waitable::READ ? EVFILT_READ : 0)
-			| (u32(flagsToWaitFor) & Waitable::READ ? EVFILT_WRITE : 0);
+			| (u32(flagsToWaitFor) & Waitable::WRITE ? EVFILT_WRITE : 0);
 	
 	struct kevent e;
 
