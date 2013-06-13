90bbe1ea0641ea0f4cf0dad6c5a7a5fa2a7122c7
igagis <igagis@1aea46d3-b44b-0410-97f0-8bd0331b130a>
2013-06-13 12:15:31 +0000

    fix

--- src/ting/WaitSet.cpp
+++ src/ting/WaitSet.cpp
@@ -150,8 +150,9 @@ void WaitSet::Change(Waitable* w, Waitable::EReadinessFlags flagsToWaitFor){
 		throw ting::Exc("WaitSet::Change(): epoll_ctl() failed");
 	}
 #elif M_OS == M_OS_MACOSX
-	int16_t filter = (u32(flagsToWaitFor) & Waitable::READ ? EVFILT_READ : 0)
-			| (u32(flagsToWaitFor) & Waitable::WRITE ? EVFILT_WRITE : 0);
+	int16_t filter = 0;
+	filter |= (u32(flagsToWaitFor) & Waitable::READ) != 0 ? EVFILT_READ : 0;
+	filter |= (u32(flagsToWaitFor) & Waitable::WRITE) != 0 ? EVFILT_WRITE : 0;
 	
 	struct kevent e;
 
