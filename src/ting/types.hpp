a58155c103e7b1a4cb539c4e68e33d973d5e222b
igagis <igagis@1aea46d3-b44b-0410-97f0-8bd0331b130a>
2014-11-01 19:21:08 +0000

    compile fix

--- src/ting/types.hpp
+++ src/ting/types.hpp
@@ -33,19 +33,20 @@ THE SOFTWARE. */
 #pragma once
 
 #include <cstdint>
+#include <cstddef>
 
 
 namespace ting{
 
 
 
-template <std::size_t type_size> struct UnsignedTypeForSize{typedef std::uint64_t Type;};
+template <size_t type_size> struct UnsignedTypeForSize{typedef std::uint64_t Type;};
 template <> struct UnsignedTypeForSize<1>{typedef std::uint8_t Type;};
 template <> struct UnsignedTypeForSize<2>{typedef std::uint16_t Type;};
 template <> struct UnsignedTypeForSize<3>{typedef std::uint32_t Type;};
 template <> struct UnsignedTypeForSize<4>{typedef std::uint32_t Type;};
 
-template <std::size_t type_size> struct SignedTypeForSize{typedef std::int64_t Type;};
+template <size_t type_size> struct SignedTypeForSize{typedef std::int64_t Type;};
 template <> struct SignedTypeForSize<1>{typedef std::int8_t Type;};
 template <> struct SignedTypeForSize<2>{typedef std::int16_t Type;};
 template <> struct SignedTypeForSize<3>{typedef std::int32_t Type;};
